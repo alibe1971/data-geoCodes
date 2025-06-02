f65d6e17296ea95acc393d5244546b8d0915a138
Andrea Liberati <25078094+alibe1971@users.noreply.github.com>
2025-06-02 20:47:59 +0100

    Clean the code

--- Lib/Builders/configBuilder.js
+++ Lib/Builders/configBuilder.js
@@ -68,47 +68,6 @@ export const configFunctions = {
         }
         Config.settings.languages.inPackage = inPackage;
 
-        // /** settings.languages.inPackage: must be a not empty object */
-        // if(
-        //     !requirements(data.settings.languages.inPackage, 'mustBeObject') ||
-        //     !requirements(data.settings.languages.inPackage, 'cannotBeEmpty')
-        // ) {
-        //     throwMex('settings.languages.inPackage', 'The property must be a not empty object');
-        // }
-        // let inPackage = {};
-        // for (let [lang, locale] of Object.entries(data.settings.languages.inPackage)) {
-        //     if( !requirements(lang, 'mustBeString') || !requirements(lang, 'regex', /^[a-z]{2}$/i) ) {
-        //         throwMex('settings.languages.inPackage',
-        //             'The key `' + lang + '` must be 2 chars length alphabetic string');
-        //     }
-        //
-        //     if( !requirements(locale, 'mustBeString') || !requirements(locale, 'cannotBeEmpty') ) {
-        //         throwMex('settings.languages.inPackage',
-        //             'The value `' + locale + '` must be a not empty string');
-        //     }
-        //     locale = refactorLanguages(locale);
-        //     if ( locale == 'error' || !requirements(locale, 'bcp47') ) {
-        //         throwMex(
-        //             'settings.languages.inPackage',
-        //             'The value `' + locale + '` non-compliant with BCP 47 format'
-        //         );
-        //     }
-        //     if (!Intl.Collator.supportedLocalesOf([locale]).length) {
-        //         throwMex('settings.languages.inPackage', 'The value `' + locale + '` is not supported');
-        //     }
-        //
-        //     lang = lang.toLowerCase();
-        //     let [locLang, locCC] = locale.split('-');
-        //     locLang = locLang.toLowerCase();
-        //     if(locLang != lang) {
-        //         throwMex('settings.languages.inPackage',
-        //             'The key `' + lang + '` does not match with the locale lang part `' + locale + '`');
-        //     }
-        //     locale = locLang + '_' + locCC.toUpperCase();
-        //     inPackage[lang] = locale;
-        // }
-        // Config.settings.languages.inPackage = inPackage;
-
         /** settings.languages.default: must be present */
         if(!Object.prototype.hasOwnProperty.call(data.settings.languages, 'default')) {
             throwMex('settings.languages.default', 'Required property is missing');
@@ -124,10 +83,6 @@ export const configFunctions = {
         Config.settings.languages.default = data.settings.languages.default;
         if(
             !Config.settings.languages.inPackage.includes(Config.settings.languages.default)
-            // !Object.prototype.hasOwnProperty.call(
-            //     Config.settings.languages.inPackage,
-            //     Config.settings.languages.default
-            // )
         ) {
             throwMex('settings.languages.default',
                 'The property value must be present in the `settings.languages.inPackage` array');
